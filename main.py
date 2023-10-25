2780df5726e2bab73d34bd9282038410364d9dfe
Edwin Feldman <edwinsfeldman@gmail.com>
2023-10-25 19:45:22 -0400

    Edwin Feldman

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    def encode(password: str) -> str:
+        if len(password) != 8 or not password.isdigit():
+            raise ValueError("The password must be an 8-digit integer string.")
+
+        encoded_password = ''.join([str((int(digit) + 3) % 10) for digit in password])
+        return encoded_password
+    pass
+
+if __name__ == "__main__":
+    main()
+    password = input("Enter 8-digit password: ")
+    encoded = encode(password)
+    print(f"Original Password: {password}")
+    print(f"Encoded Password: {encoded}")
